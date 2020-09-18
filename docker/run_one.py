b9665c184fd6b21719e2dd0cde051747d856d9df
Ian Bell <ian.bell@nist.gov>
2020-09-18 17:33:17 -0400

    Modernize running of tests via python driver

---
+++ docker/run_one.py
@@ -0,0 +1,47 @@
+import argparse, zipfile, os, glob, sys, subprocess
+
+def build_REPROP_zip(*, root, zippath):
+    """ Build a zip file with all the things to go into docker container """
+
+    def depath(fname, *, folder):
+        """ Remove the leading path, leaving just folder+file """
+        return os.path.join(folder, os.path.split(fname)[1])
+
+    def dequote(path):
+        if path[0] == '"' and path[1] == '"':
+            return path.lstrip('"').rstrip('"')
+        else:
+            return path
+
+    with zipfile.ZipFile(zippath, 'w') as rpzip:
+        for folder in ['FLUIDS','MIXTURES','FORTRAN']:
+            folder_path = os.path.join(dequote(root), folder, '*.*')
+            files = glob.glob(folder_path)
+            if not files:
+                raise ValueError('No files match '+folder_path)
+            for file in files:
+                rpzip.write(file, arcname=depath(file, folder=folder))
+
+def run_one(args):
+    """ Actually run the specified test """
+    root = args.root[0]
+    test = args.test[0]
+    build_REPROP_zip(root=root, zippath=os.path.join(test, 'REFPROP.zip'))
+    with open('build_run.log', 'w') as stdout:
+        subprocess.check_call('docker-compose up --build', cwd=test)
+
+    def debrief(recipients):
+        """ 1. Collect the things from this test, make a zip file of them """
+        """ 2. Email the results to the desired email address recipients """
+        pass
+
+if __name__ == '__main__':
+
+    sys.argv += ['--root', 'Q:/Public/Eric/install/ALPHA','--test','test']
+    parser = argparse.ArgumentParser(description='Run a specified test, and generate a zip file with perhaps relevant output')
+    parser.add_argument('--root', type=str, required=True, nargs=1, 
+        help='The root path of the REFPROP installation from which to take the folders FLUIDS, MIXTURES, and FORTRAN')
+    parser.add_argument('--test', type=str, required=True, nargs=1, 
+        help='The selected test to run', choices=['gcov','valgrind','test'])
+    args = parser.parse_args()
+    run_one(args)
--- docker/valgrind/run_valgrind.py
+++ docker/valgrind/run_valgrind.py
@@ -1,4 +1,5 @@
 import subprocess, os, sys, shutil, glob, timeit
+from ansi2html import Ansi2HTMLConverter
 
 tic = timeit.default_timer()
 output = subprocess.run('/REFPROP-tests/build/main -t', shell = True, stdout = subprocess.PIPE).stdout.decode('utf-8')
@@ -7,24 +8,28 @@ for il, line in enumerate(output.split('\n')[1::]):
 
     if not line or '[' not in line: continue
     tag = '[' + line.split('[')[1]
-    if 'Torture' in tag: continue # Don't run the Torture test (very slow)
+    if 'veryslow' in tag: continue # Don't run the veryslow tests
+#    if 'predef_mix' not in tag: continue
     root =  tag.replace('[', '').replace(']','') + '.txt'
     print(tag, ' --> ', root)
-#    continue
 
     cmd = 'valgrind --tool=memcheck --error-limit=no --track-origins=yes /REFPROP-tests/build/main ' + tag
-    with open('log_'+root,'w') as fp_stderr:
-        with open('err_'+root,'w') as fp_stdout:
+    with open('/output/log_'+root,'w') as fp_stderr:
+        with open('/output/err_'+root,'w') as fp_stdout:
             subprocess.run(cmd, shell = True, stdout = fp_stdout, stderr = fp_stderr)
-    print(open('log_'+root).readlines()[-1])
+    print(open('/output/log_'+root).readlines()[-1])
+
+    # Color the output log
+    with open('/output/log_'+root+'.html', 'w') as fp:
+        fp.write(Ansi2HTMLConverter().convert(open('/output/err_'+root).read()))
 
 # subprocess.run('for f in log_*.txt; do echo $f && tail -n 1 $f; done', shell=True, stdout = sys.stdout, stderr = sys.stderr)
 
 # Store all the outputs in zip archive
-os.makedirs('errlog')
-for g in glob.glob('log_*.txt') + glob.glob('err_*.txt'):
-    shutil.copy2(g,'errlog')
-shutil.make_archive('/output/output','zip','errlog')
+os.makedirs('/output/errlog')
+for g in glob.glob('/output/log_*.txt') + glob.glob('/output/err_*.txt'):
+    shutil.copy2(g,'/output/errlog')
+shutil.make_archive('/output/output','zip','/output/errlog')
 
 toc = timeit.default_timer()
 print('elapsed time [s]:', toc-tic)

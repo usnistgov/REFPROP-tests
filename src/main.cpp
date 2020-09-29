574081e4c55e37452e6b1d83d35d71d7b918de33
Ian Bell <ian.bell@nist.gov>
2020-09-29 15:29:35 -0400

    Add water tests
    See https://github.com/usnistgov/REFPROP-tests/issues/4

--- src/main.cpp
+++ src/main.cpp
@@ -1702,6 +1702,33 @@ public:
 };
 TEST_CASE_METHOD(WaterAlpharDerivs, "Water alphar derivatives from IAPWS95", "[water],[alphar]") { payload(); };
 
+TEST_CASE_METHOD(REFPROPDLLFixture, "Tabulated check values from IAPWS release", "[water]") {
+    // Table 7 of http://www.iapws.org/relguide/IAPWS95-2018.pdf
+    struct point { double T_K, rho_kgm3, p_MPa,cv_kJkgK,w_ms,s_kJkgK; };
+    std::vector<point> points = { {300, 0.9965560e3, 0.992418352e-1, 0.413018112e1, 0.150151914e4, 0.393062643},
+    { 300,0.1005308e4,0.200022515e2,0.406798347e1,0.153492501e4,0.387405401 },
+    { 300,0.1188202e4,0.700004704e3,0.346135580e1,0.244357992e4,0.132609616 },
+    { 500,0.4350000,0.999679423e-1,0.150817541e1,0.548314253e3,0.794488271e1 },
+    { 500,0.4532000e1,0.999938125,0.166991025e1,0.535739001e3,0.682502725e1 },
+    { 500,0.8380250e3,0.100003858e2,0.322106219e1,0.127128441e4,0.256690919e1 },
+    { 500,0.1084564e4,0.700000405e3,0.307437693e1,0.241200877e4,0.203237509e1 },
+    { 647,0.3580000e3,0.220384756e2,0.618315728e1,0.252145078e3,0.432092307e1 },
+    { 900,0.2410000,0.100062559,0.175890657e1,0.724027147e3,0.916653194e1 },
+    { 900,0.5261500e2,0.200000690e2,0.193510526e1,0.698445674e3,0.659070225e1 },
+    { 900,0.8707690e3,0.700000006e3,0.266422350e1,0.201933608e4,0.417223802e1 } };
+    auto MASS_SI = get_enum("MASS SI");
+    std::vector<double> z(1, 1.0);
+    for (auto& pt: points){
+        auto r = REFPROP("WATER", "TD", "P;CV;W;S", MASS_SI, 0, 0, pt.T_K, pt.rho_kgm3, z);
+        CAPTURE(r.herr);
+        REQUIRE(r.ierr == 0);
+        CHECK(r.Output[0] == Approx(pt.p_MPa));
+        CHECK(r.Output[1] == Approx(pt.cv_kJkgK));
+        CHECK(r.Output[2] == Approx(pt.w_ms));
+        CHECK(r.Output[3] == Approx(pt.s_kJkgK));
+    }
+}
+
 TEST_CASE_METHOD(REFPROPDLLFixture, "Check all variables that do not require state information", "[Stateless]") {
     // These need splines on...
     //std::vector<std::string> variable_names = { "TMAXT","PMAXT","DMAXT","TMAXP","PMAXP","DMAXP"};

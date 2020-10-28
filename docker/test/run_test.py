a9727b92c46e2b6fded67382f051d718f2fbcd30
Ian Bell <ian.bell@nist.gov>
2020-10-28 11:07:12 -0400

    Maybe this will help with sorting good and bad tests

--- docker/test/run_test.py
+++ docker/test/run_test.py
@@ -1,8 +1,15 @@
 import subprocess, os, sys, shutil, glob, timeit
-from ansi2html import Ansi2HTMLConverter
 
 tic = timeit.default_timer()
 output = subprocess.run('/REFPROP-tests/build/main -t', shell = True, stdout = subprocess.PIPE).stdout.decode('utf-8')
+
+# Wipe contents of output folder 
+# This script is running inside the docker container, so this should be allowed
+for g in glob.glob('/output/*'):
+    if os.path.isdir(g):
+        shutil.rmtree(g)
+    else:
+        os.remove(g)
 
 for il, line in enumerate(output.split('\n')[1::]):
 
@@ -14,26 +21,22 @@ for il, line in enumerate(output.split('\n')[1::]):
     print(tag, ' --> ', root)
 
     cmd = 'timeout 20m /REFPROP-tests/build/main ' + tag
-    with open('/output/log_'+root,'w') as fp_stderr:
-        with open('/output/err_'+root,'w') as fp_stdout:
-            fp_stdout.write('About to run command: '+cmd+'\n')
-            fp_stdout.flush()
-            subprocess.run(cmd, shell = True, stdout = fp_stdout, stderr = fp_stderr)
-    loglines = open('/output/log_'+root).readlines()
-    if loglines:
-        print(loglines[-1])
-
-    # Color the output log
-    with open('/output/log_'+root+'.html', 'w') as fp:
-        fp.write(Ansi2HTMLConverter().convert(open('/output/err_'+root).read()))
-
-# subprocess.run('for f in log_*.txt; do echo $f && tail -n 1 $f; done', shell=True, stdout = sys.stdout, stderr = sys.stderr)
+    with open('/output/err_'+root,'w') as fp_stdout:
+        fp_stdout.write('About to run command: '+cmd+'\n')
+        fp_stdout.flush()
+        subprocess.run(cmd, shell = True, stdout = fp_stdout, stderr = fp_stdout)
 
 # Store all the outputs in zip archive
-os.makedirs('/output/errlog')
-for g in glob.glob('/output/log_*.txt') + glob.glob('/output/err_*.txt'):
-    shutil.copy2(g,'/output/errlog')
-shutil.make_archive('/output/output','zip','/output/errlog')
+os.makedirs('/output/errors')
+os.makedirs('/output/ok')
+for g in glob.glob('/output/err_*.txt'):
+    is_ok = 'All tests passed' in open(g).read()
+    if is_ok:
+        shutil.move(g, '/output/ok')
+    else:
+        shutil.move(g, '/output/errors')
+shutil.make_archive('/output/errors','zip','/output/errors')
+shutil.make_archive('/output/ok','zip','/output/ok')
 
 toc = timeit.default_timer()
 print('elapsed time [s]:', toc-tic)

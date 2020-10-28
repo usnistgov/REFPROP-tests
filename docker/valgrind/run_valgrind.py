c50973a9501e31e41095e3780829326df9c418d3
Ian Bell <ian.bell@nist.gov>
2020-10-28 16:51:47 -0400

    Fix running of valgrind

--- docker/valgrind/run_valgrind.py
+++ docker/valgrind/run_valgrind.py
@@ -1,8 +1,15 @@
 import subprocess, os, sys, shutil, glob, timeit
-from ansi2html import Ansi2HTMLConverter
 
 tic = timeit.default_timer()
 output = subprocess.run('/REFPROP-tests/build/main -t', shell = True, stdout = subprocess.PIPE).stdout.decode('utf-8')
+
+# Wipe contents of output folder 
+# This script is running inside the docker container, so this should be allowed
+for g in glob.glob('/output/*'):
+    if os.path.isdir(g):
+        shutil.rmtree(g)
+    else:
+        os.remove(g)
 
 for il, line in enumerate(output.split('\n')[1::]):
 
@@ -19,17 +26,18 @@ for il, line in enumerate(output.split('\n')[1::]):
             subprocess.run(cmd, shell = True, stdout = fp_stdout, stderr = fp_stderr)
     print(open('/output/log_'+root).readlines()[-1])
 
-    # Color the output log
-    with open('/output/log_'+root+'.html', 'w') as fp:
-        fp.write(Ansi2HTMLConverter().convert(open('/output/err_'+root).read()))
-
-# subprocess.run('for f in log_*.txt; do echo $f && tail -n 1 $f; done', shell=True, stdout = sys.stdout, stderr = sys.stderr)
-
 # Store all the outputs in zip archive
-os.makedirs('/output/errlog')
-for g in glob.glob('/output/log_*.txt') + glob.glob('/output/err_*.txt'):
-    shutil.copy2(g,'/output/errlog')
-shutil.make_archive('/output/output','zip','/output/errlog')
+os.makedirs('/output/errors')
+os.makedirs('/output/ok')
+for g in glob.glob('/output/log_*.txt'):
+    test_name = os.path.split(g)[1].replace('log_','').replace('.txt','')
+    errname = f'/output/err_{test_name:s}.txt'
+    is_ok = '0 errors from 0 contexts' in open(g).read()
+    dest = '/output/ok' if is_ok else '/output/errors'
+    for name in [g, errname]:
+        shutil.move(name, dest)
+shutil.make_archive('/output/errors','zip','/output/errors')
+shutil.make_archive('/output/ok','zip','/output/ok')
 
 toc = timeit.default_timer()
 print('elapsed time [s]:', toc-tic)

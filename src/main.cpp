8d0db1e0dcbaeef126786f913492f504890dd6bb
Ian Bell <ian.bell@nist.gov>
2024-03-25 07:18:43 -0400

    Fix the 32/yf test

--- src/main.cpp
+++ src/main.cpp
@@ -458,9 +458,11 @@ TEST_CASE_METHOD(REFPROPDLLFixture, "Order of R32+yf should not matter", "[setup
     std::vector<double> z1 = { 0.6867261958191739, 0.3132738041808261 }, z2 = { 0.3132738041808261, 0.6867261958191739};
     auto r1 = REFPROP("R32*R1234YF", "TQmolar", "D", 20, 0, 0, 352.448, 0.0, z1);
     auto r2 = REFPROP("R1234YF*R32", "TQmolar", "D", 20, 0, 0, 352.448, 0.0, z2);
+    CAPTURE(r1.ierr);
     CAPTURE(r1.herr);
     CAPTURE(r2.herr);
-    CHECK_THAT(r1.Output[1], WithinAbsMatcher(r2.Output[0], 1e-11));
+    CAPTURE(r2.ierr);
+    CHECK_THAT(r1.Output[0], WithinAbsMatcher(r2.Output[0], 1e-10));
 }
 
 TEST_CASE_METHOD(REFPROPDLLFixture, "Order of ternary should not matter", "[setup],[fluidorder]") {

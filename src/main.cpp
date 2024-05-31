21faa7c2d600e853b6b8fd0620ea74dcd57ebdc7
Ian Bell <ian.bell@nist.gov>
2024-05-31 14:17:34 -0400

    Wrote test for XMASS
    See https://github.com/usnistgov/REFPROP-issues/issues/630

--- src/main.cpp
+++ src/main.cpp
@@ -1738,6 +1738,37 @@ TEST_CASE_METHOD(GETSETKTV, "Check BIP for R32 + CO2", "[BIP]") {
     }
 };
 
+TEST_CASE_METHOD(REFPROPDLLFixture, "Xmass for mixture", "[massfractions]") {
+    std::vector<double> z(20, 1);
+    SECTION("with imass=0"){
+        auto r = REFPROP("R410A.MIX", "PQ", "XMASS", 0, 0, 0, 101.325, 0.5, z);
+        CAPTURE(r.herr);
+        CHECK_THAT(r.Output[0], WithinRelMatcher(0.5, 1e-14));
+        CHECK(r.ierr != 0);
+    }
+    SECTION("with imass=1"){
+        auto r = REFPROP("R410A.MIX", "PQ", "XMASS", 0, 0, 1, 101.325, 0.5, z);
+        CAPTURE(r.herr);
+        CHECK_THAT(r.Output[0], WithinRelMatcher(0.5, 1e-14));
+        CHECK(r.ierr != 0);
+    }
+    SECTION("with imass=1"){
+        std::vector<double> z(20, 0.8); z[1] = 0.2;
+        auto r = REFPROP("R32*R125", "", "XMASS", 0, 0, 1, 101.325, 0.5, z);
+        CAPTURE(r.herr);
+        CHECK_THAT(r.Output[0], WithinRelMatcher(0.8, 1e-14));
+        CHECK(r.ierr != 0);
+    }
+    SECTION("with imass=0"){
+        std::vector<double> z(20, 0.8); z[1] = 0.2;
+        auto r = REFPROP("R32*R125", "", "XMOLE", 0, 0, 0, 101.325, 0.5, z);
+        CAPTURE(r.herr);
+        CHECK_THAT(r.Output[0], WithinRelMatcher(0.8, 1e-14));
+        CHECK(r.ierr != 0);
+    }
+};
+
+
 TEST_CASE_METHOD(REFPROPDLLFixture, "Two-phase viscosity", "[transport]") {
     std::vector<double> z(20, 1);
     auto r = REFPROP("R32 * CO2", "PQ", "ETA", 0, 0, 0, 101.325, 0.5, z);

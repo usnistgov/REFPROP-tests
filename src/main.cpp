73fec38e47d3d588c4ab30b8314dee6241b0bfb8
Ian Bell <ian.bell@nist.gov>
2024-12-19 11:41:01 -0500

    More intrusively error out if env var not set

--- src/main.cpp
+++ src/main.cpp
@@ -31,6 +31,21 @@ public:
 
     void testRunStarting(Catch::TestRunInfo const&) override {
         
+        auto str_env = [](const char* var) -> std::string {
+            const char* chr = std::getenv(var);
+            if (chr == nullptr){ return ""; }
+            return chr;
+        };
+        std::cout << "RPPREFIX: " << str_env("RPPREFIX") << std::endl;
+        std::cout << "RESOURCES: " << str_env("RESOURCES") << std::endl;
+        if (!std::filesystem::exists(str_env("RPPREFIX"))){
+            std::cerr << "RPPREFIX variable was not set to a valid path; terminating" << std::endl;
+            std::exit(100);
+        }
+        if (!std::filesystem::exists(str_env("RESOURCES"))){
+            std::cerr << "RESOURCES variable was not set to a valid path" << std::endl;
+        }
+        
         auto load_method = AbstractSharedLibraryWrapper::load_method::LOAD_LIBRARY;
         
         namespace fs = std::filesystem;
